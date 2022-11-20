744814052f11f0fb0961ceb37fd450f1ad9d840d
Jonathan Almeida <90427766+JonathanAsf@users.noreply.github.com>
2022-11-19 23:42:58 -0300

    Esfera
    Criação da bolinha e do fundo

---
+++ ping_pong.js
@@ -0,0 +1,12 @@
+let xBolinha = 300;
+let yBolinha = 200;
+let tamanhoBolinha = 22;
+
+function setup() {
+  createCanvas(600, 400);
+}
+
+function draw() {
+  background(0);
+  circle(xBolinha,yBolinha,tamanhoBolinha)
+}
