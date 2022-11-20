56aceb5037b879c4c8bc92f01593f33aae0fbd03
Jonathan Almeida <90427766+JonathanAsf@users.noreply.github.com>
2022-11-20 00:44:37 -0300

    Movimentação X e Y
    Movimentação X e Y da bolinha

--- ping_pong.js
+++ ping_pong.js
@@ -2,6 +2,10 @@ let xBolinha = 300;
 let yBolinha = 200;
 let tamanhoBolinha = 22;
 
+let velocidadexBolinha = 5
+let velocidadeyBolinha = 5
+
+
 function setup() {
   createCanvas(600, 400);
 }
@@ -9,4 +13,7 @@ function setup() {
 function draw() {
   background(0);
   circle(xBolinha,yBolinha,tamanhoBolinha)
+  xBolinha += velocidadexBolinha
+  yBolinha += velocidadeyBolinha 
+  
 }
