2538b539bfe276064e2fc1597a7f6c0f766cb4a7
Jonathan Almeida <90427766+JonathanAsf@users.noreply.github.com>
2022-11-21 09:12:59 -0300

    Colisão com borda da esfera
    Com a versão anterior a esfera ultrapassava metade do seu tamanho além da borda.
    Para resolver o problema foi feito o calculo do raio, visto que o ponto de colisão da esfera estava no centro, logo, somando com o raio, a esfera colide corretamente entre os seus limites e da tela.

--- ping_pong.js
+++ ping_pong.js
@@ -1,26 +1,28 @@
+//Variáveis da esfera
 let xBolinha = 300;
 let yBolinha = 200;
-let tamanhoBolinha = 22;
-
+let diametro = 22;
+let raio = diametro /2;
+//Velocidade da esfera
 let velocidadexBolinha = 5
 let velocidadeyBolinha = 5
 
-
+//Background
 function setup() {
   createCanvas(600, 400);
 }
-
+//Função principal
 function draw() {
   background(0);
-  circle(xBolinha,yBolinha,tamanhoBolinha);
-  xBolinha += velocidadexBolinha;
+  circle(xBolinha,yBolinha,diametro);
+  //xBolinha += velocidadexBolinha;
   yBolinha += velocidadeyBolinha;
-  
-  if(xBolinha > width || xBolinha < 0){
+  //Limite de borda horizontal
+  if(xBolinha + raio > width || xBolinha - raio < 0){
     velocidadexBolinha *= -1;
   }
-  
-  if(yBolinha > height|| yBolinha < 0){
+  //Limite de borda vertical
+  if(yBolinha + raio> height|| yBolinha - raio < 0){
     velocidadeyBolinha *= -1;
   }
 }
